814fd903f63c7862973e025961aa237b30411cee
Abhisheksb1003 <abhishekbikkannavar1@gmail.com>
2023-11-03 13:39:07 +0530

    Children Props and Behind the Scenes

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,24 +1,20 @@
-import ExpenseDate from "./ExpenseDate";
-import ExpenseDetail from "./ExpenseDetail";
+import ExpenseDate from './ExpenseDate';
+import Card from './Card';
+
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-
   return (
-   <div className='expense-item'>
-<ExpenseDate date={props.date}/>
-<ExpenseDetail
-title={props.title}
-location={props.location}
-amount={props.amount}
-/>
-    
-    <div className='expense-item__description'>
-      <h2>{props.title}</h2>
-      <h2>{props.location}</h2>
-      <div className='expense-item__price'>${props.amount}</div>
-    </div>
-   </div>
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      
+
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <h2>{props.location}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </Card>
   );
 }
 
