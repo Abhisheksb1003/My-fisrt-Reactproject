dd012616912b0ccf448b803bd894bfcc07b71bfa
Abhisheksb1003 <abhishekbikkannavar1@gmail.com>
2023-11-03 17:09:19 +0530

    Adding Event Listeners made easy

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,6 +4,10 @@ import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem=(props)=> {
+
+  const clcikHandler=()=>{
+    alert('Expense Deleted')
+  }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -14,6 +18,7 @@ const ExpenseItem=(props)=> {
         <h2>{props.location}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clcikHandler}>Delete Expense</button>
     </Card>
   );
 }
