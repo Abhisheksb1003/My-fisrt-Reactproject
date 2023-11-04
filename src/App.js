57e0ccbd3c479755dd17db07b524be65ffeda5e0
Abhisheksb1003 <abhishekbikkannavar1@gmail.com>
2023-11-04 14:28:04 +0530

    New Making the expense form input visible on screen

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import React, {useState} from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
 const App=() => {
-  const expenses = [
+  const [expenses,setExpenses]=useState([
     {
       id: 'e1',
       title: 'Toilet Paper',
@@ -31,11 +32,14 @@ const App=() => {
       date: new Date(2021, 5, 12),
       location: "Mysore"
     },
-  ];
+  ]);
 
   const addExpenseHandler=expense=>{
-    console.log('In App.js');
-    console.log(expense)
+   const newExpense={
+    id: Math.random().toString(),
+    ...expense
+   };
+   setExpenses((prevExpenses)=>[newExpense, ...prevExpenses])
   }
 
   return (
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,34 +1,34 @@
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
-import React,{useState} from 'react'
+
 
 const ExpenseItem=(props)=> {
 
-  const [title,setTitle]= useState(props.title)
+  // const [title,setTitle]= useState(props.title)
 
-  const [amount,setAmount]= useState(props.amount)
+  // const [amount,setAmount]= useState(props.amount)
 
-  const clcikHandler=()=>{
-    setTitle('Update')
-    console.log(title)
-  }
-  const clickButton1=()=>{
-    setAmount('100')
+  // const clcikHandler=()=>{
+  //   setTitle('Update')
+  //   console.log(title)
+  // }
+  // const clickButton1=()=>{
+  //   setAmount('100')
     
-  }
+  // }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       
 
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <h2>{props.location}</h2>
-        <div className="expense-item__price">${amount}</div>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clcikHandler}>Change Title</button>
-      <button onClick={clickButton1}>Change Amount</button>
+      {/* <button onClick={clcikHandler}>Change Title</button>
+      <button onClick={clickButton1}>Change Amount</button> */}
     </Card>
   );
 }
